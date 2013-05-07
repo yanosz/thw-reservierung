235de0650fb5067f8fe7c0e8513cbcc18cc93ddd
kaleng <kaleng@zoho.com>
2013-05-07 22:13:10 +0200

    jQuery.noconflict

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -1,14 +1,15 @@
-$(document).ready(function(){
-	$("table.list-table").on("click", "th", function(){
-		if($(this).hasClass("0"))
-			$("table.list-table").tablesorter({sortList:[[0,0]]});
-		else if($(this).hasClass("1"))
-			$("table.list-table").tablesorter({sortList:[[1,0]]});
-		else if($(this).hasClass("2"))
-			$("table.list-table").tablesorter({sortList:[[2,0]]});
-		else if($(this).hasClass("3"))
-			$("table.list-table").tablesorter({sortList:[[3,0]]});
-		else if($(this).hasClass("4"))
-			$("table.list-table").tablesorter({sortList:[[4,0]]});
+jQuery.noConflict();
+jQuery(document).ready(function(){
+	jQuery("table.list-table").on("click", "th", function(){
+		if(jQuery(this).hasClass("0"))
+			jQuery("table.list-table").tablesorter({sortList:[[0,0]]});
+		else if(jQuery(this).hasClass("1"))
+			jQuery("table.list-table").tablesorter({sortList:[[1,0]]});
+		else if(jQuery(this).hasClass("2"))
+			jQuery("table.list-table").tablesorter({sortList:[[2,0]]});
+		else if(jQuery(this).hasClass("3"))
+			jQuery("table.list-table").tablesorter({sortList:[[3,0]]});
+		else if(jQuery(this).hasClass("4"))
+			jQuery("table.list-table").tablesorter({sortList:[[4,0]]});
 	});
 })
