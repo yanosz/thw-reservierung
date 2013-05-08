09f18d9531385d32cd219e68802cdf69897e4f17
kaleng <kaleng@zoho.com>
2013-05-08 14:08:14 +0200

    + SortierungsIcons, spezielle Sortierung bei rentals

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -1,15 +1,16 @@
 jQuery.noConflict();
 jQuery(document).ready(function(){
-	jQuery("table.list-table").on("click", "th", function(){
-		if(jQuery(this).hasClass("0"))
-			jQuery("table.list-table").tablesorter({sortList:[[0,0]]});
-		else if(jQuery(this).hasClass("1"))
-			jQuery("table.list-table").tablesorter({sortList:[[1,0]]});
-		else if(jQuery(this).hasClass("2"))
-			jQuery("table.list-table").tablesorter({sortList:[[2,0]]});
-		else if(jQuery(this).hasClass("3"))
-			jQuery("table.list-table").tablesorter({sortList:[[3,0]]});
-		else if(jQuery(this).hasClass("4"))
-			jQuery("table.list-table").tablesorter({sortList:[[4,0]]});
+	if(jQuery("#list-table").hasClass("rentals"))
+		sortOrder = [[2,0][3,0][0,0]]
+	else
+		sortOrder = [[0,0]]
+
+	jQuery("#list-table").tablesorter({ 
+		sortList: sortOrder,
+		dateFormat : "ddmmyyyy",
+		widgets: ['zebra'],
+		widgetOptions: {
+			zebra : ["even", "odd"]
+		}	
 	});
 })
